1ec98bdcd1fc49599e78974a76793175a11b79fd
qulogen <kuro.rltb@gmail.com>
2022-10-28 19:16:06 +0900

    basyo

---
+++ 1-3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void F()
+{
+    printf("ふるいけや");
+}
+
+void K()
+{
+    printf("かわずとびこむ");
+}
+
+void M()
+{
+    printf("みずのおと");
+}
+
+int main(void)
+{
+    F();
+    K();
+    M(); 
+    return 0;
+}
